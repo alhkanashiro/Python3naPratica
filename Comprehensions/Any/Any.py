7b52d40070636a397609d39a10156561c9495a6d
André Luiz H. Kanashiro <alhkanashiro@gmail.com>
2020-03-27 15:25:48 -0300

    Aqui começa meus primeiros passos para uso da linguagem de programação Python

---
+++ Comprehensions/Any/Any.py
@@ -0,0 +1,6 @@
+listaVerdade = [True, True, True]
+listaFalsa = [False, False, False]
+listaMista = [True, False, True]
+# any(listaVerdade)
+# any(listaFalsa)
+# any(listaMista)
---
+++ For/loopFor.py
@@ -0,0 +1,4 @@
+dias_da_semana = ['segunda', 'terça', 'quarta', 'quinta', 'sexta','sabado', 'domingo']
+for dia in dias_da_semana:
+    print(dia)
+    
